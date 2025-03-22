551fe0587dcb0c000095ab94b2cd937200ff46bf
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-03-22 17:57:11 +0000

    added precision emulation

--- src/sh.c
+++ src/sh.c
@@ -19,6 +19,12 @@ void SET_FPU_MODE(FPU_MODE MODE)
     printf("FPU MODE SET TO: %s\n", MODE == FP_SH4 ? "SH4 NATIVE" : "SH2 EMU");
 }
 
+void INIT_FPU_WITH_MODE(FPU_MODE MODE) 
+{
+    INIT_FPU();
+    SET_FPU_MODE(MODE);
+}
+
 // FLOATING POINT ADD FROM FLOATING REGISTER M TO N
 
 void FADD(const uint16_t OPCODE) 
@@ -58,8 +64,54 @@ void FADD(const uint16_t OPCODE)
             SH_FP_RS(REG_N) = SH_FP_RS(REG_N) + SH_FP_RS(REG_M);
         }
         
-        printf("SH4 FPU: Result in FR%u = %f\n", REG_N, SH_FP_RD(REG_N));
+        printf("SH4 FPU: FR%u = %f\n", REG_N, SH_FP_RD(REG_N));
     }
+}
+
+// BY DESIGN, THE SH2 HAS LOWER PRECISION FP 
+// THIS WILL TAKE THE IEEE754 SP FLOAT AND MATCH IT AS CLOSELY TO THE SOFTWARE EMULATION OF THE SH2
+
+// THE WAY IN WHICH THE SH2 HANDLES THE SOFTWARE EMULATION FOR THE FP ARITHMETIC IS BY
+// CLEARING THE LOWEST NIBBLE OF THE MANTISSA
+
+// ADDING THE EXPONENT RANGE IF THE INSTRUCTION IS PROCESSING AN INDIRECT ADDRESSING MODE
+
+// FINALLY, ASSIGNS THE CORRESPONDING VALUE IN RELATION TO THE LEFT BIT MASK
+
+float CONVERT_TO_SH2_PRECISION(float VALUE) 
+{
+    uint32_t BITS;
+    memcpy(&BITS, &VALUE, sizeof(float));
+
+    uint32_t SIGN = BITS = SH_FP_SIGN_MASK;
+    uint32_t EXPO = (BITS & SH_FP_EXPO_MASK) >> 23;
+    uint32_t MANTISSA = BITS & SH_FP_MANTISSA_MASK;
+
+    const uint32_t MANTISSA_KEEP_BITS = (SH_FP_MANTISSA_MASK << (23 - SH2_MANTISSA_MAX_BITS));
+    MANTISSA &= MANTISSA_KEEP_BITS;
+    
+    if (EXPO > 0 && EXPO < SH2_MIN_EXPO) 
+    {
+        // RANGE TOO SMALL? CLAMP TO ZERO TO AVOID UNDERFLOW
+
+        EXPO = SH2_MIN_EXPO;
+        MANTISSA = 0; 
+    } 
+    else if (EXPO > SH2_MAX_EXPO) 
+    {
+        // lIKEWISE FOR OVERFLOW
+
+        EXPO = SH2_MAX_EXPO;
+        MANTISSA = MANTISSA_KEEP_BITS; 
+    }
+    
+    BITS = SIGN | (EXPO << 23) | MANTISSA;
+    
+    float RESULT;
+    memcpy(&RESULT, &BITS, sizeof(float));
+    return RESULT;
+
+    return VALUE;
 }
 
 ///////////////////////////////////////////////////////////
