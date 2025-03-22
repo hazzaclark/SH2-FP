737603a650ee81991c0bcd4d50fcd32b2953e04d
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-03-22 00:20:13 +0000

    updated to work with a proper array
    being as though i cant cast raw pointers for unalloc'd structs

--- inc/sh_types.h
+++ inc/sh_types.h
@@ -11,12 +11,14 @@
 
 #include "sh.h"
 
+static float SH_FP_REGISTERS[16];
+
 #define         SH_FP_RS_BIN(VALUE)         SH_BASE_T->FR[(VALUE)]
 #define         SH_FP_XS_BIN(VALUE)         SH_BASE_T->XR[(VALUE)]
-#define         SH_FP_RS(VALUE)             * (float *) (SH_BASE_T->FR + (VALUE))
-#define         SH_FP_RD(VALUE)             * (double *) (SH_BASE_T->FR + (VALUE))
-#define         SH_FP_XFS(VALUE)            *( (float *)(SH_BASE_T->XR + (VALUE)) )
-#define         SH_FP_XFD(VALUE)            *( (double *)(SH_BASE_T->XR + (VALUE)) )
+#define         SH_FP_RS(VALUE)             (SH_FP_REGISTERS[VALUE])
+#define         SH_FP_RD(VALUE)             (SH_FP_REGISTERS[VALUE])
+#define         SH_FP_XFS(VALUE)            (SH_FP_REGISTERS[VALUE] + SH_BASE_T->XR)
+#define         SH_FP_XFD(VALUE)            (SH_FP_REGISTERS[VALUE] + SH_BASE_T->XR)
 
 void FADD(const uint16_t OPCODE);
 void FSUB(const uint16_t OPCODE);
